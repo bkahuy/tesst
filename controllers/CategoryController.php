0b96de8db5f852f3bdb4b296e4b67571c594fcf9
Bùi Khắc Huy <buikhachuy003@gmail.com>
2024-12-07 21:15:34 +0700

    change

--- controllers/CategoryController.php
+++ controllers/CategoryController.php
@@ -2,4 +2,16 @@
 class CategoryController{
     private $id;
     private $name;
+
+    /**
+     * @param $id
+     * @param $name
+     */
+    public function __construct($id, $name)
+    {
+        $this->id = $id;
+        $this->name = $name;
+    }
+
+
 }
