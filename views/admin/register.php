77dd5d602d7866494f213ce54f20b15075ce5410
Bùi Khắc Huy <buikhachuy003@gmail.com>
2024-12-08 18:43:48 +0700

    temp

---
+++ views/admin/register.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form method="POST" action="<?php echo DOMAIN . 'index.php?controller=user&action=register'; ?>">
+    <input type="text" name="username" placeholder="Username" required>
+    <input type="password" name="password" placeholder="Password" required>
+    <select name="role" id="role">
+        <option value="admin">admin</option>
+        <option value="user">user</option>
+    </select>
+    <button type="submit">Đăng ký</button>
+</form>
+
+</body>
+</html>
